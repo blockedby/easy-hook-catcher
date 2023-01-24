b2be220841bdbb3ffb28a94ca1b6486aa68c6eed
Alex Green <kasnis12@gmail.com>
2023-01-24 15:59:23 +0100

    -_--__--___-

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // Require express and body-parser
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 // Initialize express and define a port
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 // Tell express to use body-parser's JSON parsing
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+
+let requests = []; // array to store console logs
+
 
 app.post("/hook", (req, res) => {
-  console.log("Webhook recieved:")
-  console.log(req + "\n ___________ \n") // Call your action on the request here
-  res.status(200).end() // Responding is important
-})
+  console.log("Webhook recieved:");
+  console.log(JSON.stringify(req.body) + "\n ___________ \n"); // Call your action on the request here
+  requests.push(JSON.stringify(req.body)); // add the log to the array
+  res.status(200).end(); // Responding is important
+});
 
+
+app.get("/", (req, res) => {
+  res.render("index", { requests: requests });
+});
+
+app.post("/console", (req, res) => {
+  requests.push(JSON.stringify(req)); // add the log to the array
+  logs.push(req.query.log); // add the log to the array
+  // res.redirect('/');
+});
 
 // Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.set("view engine", "ejs"); //set the template engine
